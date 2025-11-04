07d740534fc9ba6ec140ee2975f437e8c1a6fac7
Paula-Refaat <poularefaat86@gmail.com>
2025-11-05 01:17:44 +0200

    🎉 Initialize project with Express, TypeScript, and Prisma setup

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+import { PORT } from "./scripts";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
