036ad89e94135ee15844a576f856eb4b77172390
Paula-Refaat <poularefaat86@gmail.com>
2025-11-07 18:52:51 +0200

    feat: update user schema to require role and implement role enum in Prisma

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -9,29 +9,35 @@ const authMiddleware = async (
   res: Response,
   next: NextFunction
 ) => {
-  // 1. extract the token from header
   const token = req.headers.authorization;
-  // 2. if the token not present, thew an error as unauthorized
   if (!token) {
-    next(new UnauthorizeException("Unauthorized", ErrorCode.UNAUTHORIZED));
+    return next(
+      new UnauthorizeException("Unauthorized", ErrorCode.UNAUTHORIZED)
+    );
   }
+
   try {
-    // 3. if the token is present, verify that token and extract the payload
     const payload = jwt.verify(token as string, JWT_SECRET) as any;
 
-    // 4. to get the user from the payload
     const user = await prismaClient.user.findFirst({
       where: { id: payload.userId },
     });
+
     if (!user) {
-      next(new UnauthorizeException("Unauthorized", ErrorCode.UNAUTHORIZED));
+      return next(
+        new UnauthorizeException("Unauthorized", ErrorCode.UNAUTHORIZED)
+      );
     }
-    // 5. to attach the user to the current request
+
+    // attach the user to the request
     req.user = user;
+
+    // ✅ لازم تنادي next() هنا
     next();
   } catch (error) {
     next(new UnauthorizeException("Unauthorized", ErrorCode.UNAUTHORIZED));
   }
 };
+
 
 export default authMiddleware;
