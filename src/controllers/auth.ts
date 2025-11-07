b72c41ad9e795725cb5a045a5c02195c73bd7152
Paula-Refaat <poularefaat86@gmail.com>
2025-11-07 15:50:37 +0200

    feat: enhance error handling in authentication routes and add NotFoundException

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -7,42 +7,31 @@ import { BadRequestException } from "../exceptions/bad-request";
 import { ErrorCode } from "../exceptions/root";
 import { UnprocessableEntity } from "../exceptions/validation";
 import { SignUpSchema } from "../schema/users";
+import { NotFoundException } from "../exceptions/not-found";
 export const signup = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    SignUpSchema.parse(req.body);
-    const { name, email, password, phone, address } = req.body;
-    let user = await prismaClient.user.findFirst({ where: { email } });
-    if (user) {
-      return next(
-        new BadRequestException(
-          "User Already Exists",
-          ErrorCode.USER_ALREADY_EXISTS
-        )
-      );
-    }
-    user = await prismaClient.user.create({
-      data: {
-        name,
-        email,
-        password: hashSync(password, 10),
-        phone,
-        address,
-      },
-    });
-    res.json(user);
-  } catch (err) {
-    next(
-      new UnprocessableEntity(
-        (err as any)?.issues || [],
-        "Unprocessable Entity",
-        ErrorCode.UNPROCESSABLE_ENTITY
-      )
+  SignUpSchema.parse(req.body);
+  const { name, email, password, phone, address } = req.body;
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  if (user) {
+    new BadRequestException(
+      "User Already Exists",
+      ErrorCode.USER_ALREADY_EXISTS
     );
   }
+  user = await prismaClient.user.create({
+    data: {
+      name,
+      email,
+      password: hashSync(password, 10),
+      phone,
+      address,
+    },
+  });
+  res.json(user);
 };
 
 export const login = async (
@@ -53,14 +42,12 @@ export const login = async (
   const { email, password } = req.body;
   let user = await prismaClient.user.findFirst({ where: { email } });
   if (!user) {
-    throw new Error("User Not Found");
+    throw new NotFoundException("User Not Found", ErrorCode.USER_NOT_FOUND);
   }
   if (!compareSync(password, user.password)) {
-    return next(
-      new BadRequestException(
-        "Password is Incorrect",
-        ErrorCode.INCORRECT_PASSWORD
-      )
+    new BadRequestException(
+      "Password is Incorrect",
+      ErrorCode.INCORRECT_PASSWORD
     );
   }
   const token = jwt.sign({ userId: user.id }, JWT_SECRET);
